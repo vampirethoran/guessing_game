268725f2a4642d4fa73b53ec0e40b5f5c079bdcd
bijoy <bijoy.beneathatree@gmail.com>
2025-12-12 08:35:15 +0530

    Update guessing game prompt and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("GUESS DA NUMBER!!!");
+    println!("What's your guess?");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("That's not a valid guess you wanker!");
+
+    println!("You guessed: {guess}");
 }
