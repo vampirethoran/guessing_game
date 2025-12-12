ef40a43450a2667fd94d47d18960c3516a47f51d
bijoy <bijoy.beneathatree@gmail.com>
2025-12-12 08:39:07 +0530

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::io;
+use std::io; //import io -  input/output from std - the standard library
 
 fn main() {
-    println!("GUESS DA NUMBER!!!");
+    println!("GUESS DA NUMBER!!!"); //println! - print a string to the console
     println!("What's your guess?");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //let mut guess = String::new(); - create a mutable variable called guess that is a new empty string
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("That's not a valid guess you wanker!");
+    io::stdin() //io::stdin() - read input from the standard input (console)
+        .read_line(&mut guess) //read_line(&mut guess) - read a line from the standard input and store it in the guess variable
+        .expect("That's not a valid guess you wanker!"); //expect("That's not a valid guess you wanker!") - if the read_line fails, print the error message
 
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess}"); //println!("You guessed: {guess}") - print the guess variable to the console
 }
