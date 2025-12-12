cd5ed988e29cb4f5a697645428ea829ee9d08a2a
bijoy <bijoy.beneathatree@gmail.com>
2025-12-12 08:41:38 +0530

    update comments

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     println!("GUESS DA NUMBER!!!"); //println! - print a string to the console
     println!("What's your guess?");
 
-    let mut guess = String::new(); //let mut guess = String::new(); - create a mutable variable called guess that is a new empty string
+    let mut guess = String::new(); // create a mutable variable called guess that is a new empty string
 
-    io::stdin() //io::stdin() - read input from the standard input (console)
-        .read_line(&mut guess) //read_line(&mut guess) - read a line from the standard input and store it in the guess variable
-        .expect("That's not a valid guess you wanker!"); //expect("That's not a valid guess you wanker!") - if the read_line fails, print the error message
+    io::stdin() // read input from the standard input (console)
+        .read_line(&mut guess) // read a line from the standard input and store it in the guess variable
+        .expect("That's not a valid guess you wanker!"); // expect("That's not a valid guess you wanker!") - if the read_line fails, print the error message
 
-    println!("You guessed: {guess}"); //println!("You guessed: {guess}") - print the guess variable to the console
+    println!("You guessed: {guess}"); // print the guess variable to the console
 }
